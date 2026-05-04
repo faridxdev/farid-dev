8aefd82cacfad0c293ad71a79a2649c17c1690f3
farid <2zzvc86k9y@privaterelay.appleid.com>
2026-05-04 11:27:34 +0000

    suppression de superuser dans build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-# Création automatique du superutilisateur
-python manage.py createsuperuser --noinput || true
